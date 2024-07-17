d36107eabfecf70a2124088f346e38f89c92887c
SandyTruant <190949224@qq.com>
2024-07-18 00:15:28 +0800

    Combined pe and multiplier and corrected the timing error.

--- processing_element.v
+++ processing_element.v
@@ -1,16 +1,18 @@
-module multiplier (
-    input clk,
-    input rst,
-    input ready,
-    input signed [7:0] data1,
-    input signed [7:0] data2,
-    output reg [15:0] result,
-    output reg done
+module ProcessingElement(
+    input wire clk,
+    input wire rst,
+    input wire signed [7:0] in_data1,
+    input wire signed [7:0] in_data2,
+    input wire ready,
+    output reg signed [22:0] result,
+    output reg done,
+    output reg [7:0] out_data1,
+    output reg [7:0] out_data2
 );
 
-localparam IDLE = 0, CALC = 1;
+localparam IDLE = 0, CALC = 1, DONE1 = 2, DONE2 = 3;
 
-reg state;
+reg [1:0] state;
 reg [3:0] data2_addr;
 reg signed [15:0] temp1;
 reg signed [7:0] temp2;
@@ -19,24 +21,26 @@ reg signed [15:0] temp3;
 always @(posedge clk) begin
     if (rst) begin
         state <= IDLE;
+        result <= 23'b0;
     end
     else begin
         case (state)
             IDLE: begin
                 if (ready) begin
                     state <= CALC;
+                    data2_addr <= 4'b0;
+                    temp1 <= $signed(in_data1);
+                    temp2 <= $signed(in_data2);
                     temp3 <= 16'b0;
-                    done <= 1'b0;
-                    data2_addr <= 3'b0;
-                    temp1 <= $signed(data1);
-                    temp2 <= $signed(data2);
                 end
             end
             CALC: begin
                 if (data2_addr == 8) begin
-                    state <= IDLE;
+                    state <= DONE1;
                     done <= 1'b1;
-                    result <= temp3;
+                    result <= result + $signed(temp3);
+                    out_data1 <= in_data1;
+                    out_data2 <= in_data2;
                 end
                 else begin
                     if (temp2[data2_addr]) begin
@@ -49,6 +53,13 @@ always @(posedge clk) begin
                     end
                     data2_addr <= data2_addr + 1;
                 end
+            end
+            DONE1: begin
+                state <= DONE2;
+                done <= 0;
+            end
+            DONE2: begin
+                state <= IDLE;
             end
         endcase
     end
